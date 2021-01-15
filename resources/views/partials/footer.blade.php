75746e3b52afba01c4461c24d6140bf0624e55c5
rednum <rednum89@gmail.com>
2021-01-15 19:34:26 +0100

    home page skeleton, header, footer, navigation, basic responsive styling

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,38 @@
+<footer>
+    <div class="w-full bg-gray-700 text-gray-200 flex flex-col items-center">
+        <div class="px-16 sm:px-10 py-3 w-full sm:w-10/12 lg:w-7/12 bg-gray-700 border-b border-white border-opacity-50">
+            Sitemap
+        </div>
+        <div class="flex justify-around sm:justify-start pt-4 px-10 pb-16 w-full sm:w-10/12 lg:w-7/12 bg-gray-700">
+            <div class="pr-0 sm:pr-24">
+                <div class="text-xl font-bold mb-2 text-indigo-400">Blog</div>
+                <ul>
+                    <li class="border-l border-white border-opacity-50 pl-1 pb-2 text-sm">Home</li>
+                    <li class="border-l border-white border-opacity-50 pl-1 pb-2 text-sm">Categories</li>
+                    <li class="border-l border-white border-opacity-50 pl-1 text-sm">About</li>
+                </ul>
+            </div>
+            <div class="pr-0 sm:pr-24">
+                <div class="text-xl font-bold mb-2 text-green-400">Account</div>
+                <ul>
+                    <li class="border-l border-white border-opacity-50 pl-1 pb-2 text-sm">Register</li>
+                    <li class="border-l border-white border-opacity-50 pl-1 pb-2 text-sm">Login</li>
+                    <li class="border-l border-white border-opacity-50 pl-1 text-sm">Dashboard</li>
+                </ul>
+            </div>
+            <div class="">
+                <div class="text-xl font-bold mb-2 text-blue-400">
+                    Github
+                </div>
+                <div class="text-xl font-bold mb-2 text-yellow-400">
+                    Portfolio
+                </div>
+            </div>
+        </div>
+        <div class="flex justify-center pt-2 pb-4 border-t border-white border-opacity-30 w-full sm:w-10/12 lg:w-7/12 bg-gray-700">
+            Copyright &#169; 2021&#160;<a href="" class="font-bold">gbcode</a>. All rights reserved.
+        </div>
+    </div>
+</footer>
+</body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">    
+    <title>Blog-a-lot</title>
+    <style>
+        html{
+            font-size: 12px
+        }
+        @media (min-width: 640px) 
+        { 
+            html{
+                font-size: 16px
+            }     
+        }
+}
+    </style>
+</head>
+<body class="bg-gray-100">
+    <header class="flex flex-col items-center">
+        <div class="flex justify-between items-center w-full py-4">
+            <div class="sm:w-4/12"></div>
+            <h1 class="sm:text-center w-8/12 text-3xl sm:text-5xl pl-6 sm:pl-0 text-gray-600 font-mono tracking-wide">
+                Blog-a-lot
+            </h1>
+            <div class="w-4/12 text-gray-800 flex flex-col justify-center items-end">
+                <div class="flex flex-col text-sm sm:text-lg items-center pr-6 sm:pr-12 tracking-widest">
+                    <a href="" class=" border-b border-gray-400 pb-1">
+                        LOGIN
+                    </a>
+                    <a href="" class="mt-1">
+                        REGISTER
+                    </a>
+                </div>
+            </div>
+        </div>
+        <nav class="w-full border-t border-b border-gray-300">
+            <ul class="flex py-4 justify-center bg-gray-200 font-serif">
+                <li class="px-6 text-xl underline border-r-2 border-gray-300">
+                    <a href="">Home</a>
+                </li>
+                <li class="px-6 text-xl underline border-r-2 border-gray-300">
+                    <a href="">Categories</a>
+                </li>
+                <li class="px-6 text-xl underline">
+                    <a href="">About</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
